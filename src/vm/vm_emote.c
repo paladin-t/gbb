9645dcf187be479ab57c2505f02f423082d2018d
tony <hellotony521@qq.com>
2025-10-24 14:16:06 +0800

    +Added support for emotes with 8x8px-per-tile based sprites.

--- src/vm/vm_emote.c
+++ src/vm/vm_emote.c
@@ -13,169 +13,353 @@ BANKREF(VM_EMOTE)
 #define EMOTE_TOTAL_FRAME_COUNT    60
 #define EMOTE_BOUNCE_FRAME_COUNT   15
 
+// Motion offsets.
 const INT8 EMOTE_OFFSETS[] = {
      2,  1,  0,
     -1, -2, -3, -4, -5, -6,
     -5, -4, -3, -2, -1,  0,
 };
 
-const metasprite_t EMOTE_FRAME0[] = {
+// Frames for palette 0.
+const metasprite_t EMOTE_8x16_FRAME0[] = {
     METASPR_ITEM(0, 0, 0, 0x00), METASPR_ITEM(0, 8, 2, 0x00),
     METASPR_TERM
 };
-const metasprite_t EMOTE_FRAME_MIRRORED0[] = {
+const metasprite_t * const EMOTE_8x16_METASPRITE0[] = {
+    EMOTE_8x16_FRAME0,
+    NULL
+};
+const metasprite_t EMOTE_8x16_MIRRORED_FRAME0[] = {
     METASPR_ITEM(0, 0, 0, 0x00), METASPR_ITEM(0, 8, 0, 0x20),
     METASPR_TERM
 };
-const metasprite_t * const EMOTE_METASPRITE0[] = {
-    EMOTE_FRAME0,
-    NULL
-};
-const metasprite_t * const EMOTE_METASPRITE_MIRRORED0[] = {
-    EMOTE_FRAME_MIRRORED0,
+const metasprite_t * const EMOTE_8x16_MIRRORED_METASPRITE0[] = {
+    EMOTE_8x16_MIRRORED_FRAME0,
     NULL
 };
 
-const metasprite_t EMOTE_FRAME1[] = {
+const metasprite_t EMOTE_8x8_FRAME0[] = {
+    METASPR_ITEM(0, 0, 0, 0x00), METASPR_ITEM(0, 8, 2, 0x00),
+    METASPR_ITEM(8, -8, 1, 0x00), METASPR_ITEM(0, 8, 3, 0x00),
+    METASPR_TERM
+};
+const metasprite_t * const EMOTE_8x8_METASPRITE0[] = {
+    EMOTE_8x8_FRAME0,
+    NULL
+};
+const metasprite_t EMOTE_8x8_MIRRORED_FRAME0[] = {
+    METASPR_ITEM(0, 0, 0, 0x00), METASPR_ITEM(0, 8, 0, 0x20),
+    METASPR_ITEM(8, -8, 1, 0x00), METASPR_ITEM(0, 8, 1, 0x20),
+    METASPR_TERM
+};
+const metasprite_t * const EMOTE_8x8_MIRRORED_METASPRITE0[] = {
+    EMOTE_8x8_MIRRORED_FRAME0,
+    NULL
+};
+
+// Frames for palette 1.
+const metasprite_t EMOTE_8x16_FRAME1[] = {
     METASPR_ITEM(0, 0, 0, 0x01), METASPR_ITEM(0, 8, 2, 0x01),
     METASPR_TERM
 };
-const metasprite_t EMOTE_FRAME_MIRRORED1[] = {
+const metasprite_t * const EMOTE_8x16_METASPRITE1[] = {
+    EMOTE_8x16_FRAME1,
+    NULL
+};
+const metasprite_t EMOTE_8x16_MIRRORED_FRAME1[] = {
     METASPR_ITEM(0, 0, 0, 0x01), METASPR_ITEM(0, 8, 0, 0x21),
     METASPR_TERM
 };
-const metasprite_t * const EMOTE_METASPRITE1[] = {
-    EMOTE_FRAME1,
-    NULL
-};
-const metasprite_t * const EMOTE_METASPRITE_MIRRORED1[] = {
-    EMOTE_FRAME_MIRRORED1,
+const metasprite_t * const EMOTE_8x16_MIRRORED_METASPRITE1[] = {
+    EMOTE_8x16_MIRRORED_FRAME1,
     NULL
 };
 
-const metasprite_t EMOTE_FRAME2[] = {
+const metasprite_t EMOTE_8x8_FRAME1[] = {
+    METASPR_ITEM(0, 0, 0, 0x01), METASPR_ITEM(0, 8, 2, 0x01),
+    METASPR_ITEM(8, -8, 1, 0x01), METASPR_ITEM(0, 8, 3, 0x01),
+    METASPR_TERM
+};
+const metasprite_t * const EMOTE_8x8_METASPRITE1[] = {
+    EMOTE_8x8_FRAME1,
+    NULL
+};
+const metasprite_t EMOTE_8x8_MIRRORED_FRAME1[] = {
+    METASPR_ITEM(0, 0, 0, 0x01), METASPR_ITEM(0, 8, 0, 0x21),
+    METASPR_ITEM(8, -8, 1, 0x01), METASPR_ITEM(0, 8, 1, 0x21),
+    METASPR_TERM
+};
+const metasprite_t * const EMOTE_8x8_MIRRORED_METASPRITE1[] = {
+    EMOTE_8x8_MIRRORED_FRAME1,
+    NULL
+};
+
+// Frames for palette 2.
+const metasprite_t EMOTE_8x16_FRAME2[] = {
     METASPR_ITEM(0, 0, 0, 0x02), METASPR_ITEM(0, 8, 2, 0x02),
     METASPR_TERM
 };
-const metasprite_t EMOTE_FRAME_MIRRORED2[] = {
+const metasprite_t * const EMOTE_8x16_METASPRITE2[] = {
+    EMOTE_8x16_FRAME2,
+    NULL
+};
+const metasprite_t EMOTE_8x16_MIRRORED_FRAME2[] = {
     METASPR_ITEM(0, 0, 0, 0x02), METASPR_ITEM(0, 8, 0, 0x22),
     METASPR_TERM
 };
-const metasprite_t * const EMOTE_METASPRITE2[] = {
-    EMOTE_FRAME2,
-    NULL
-};
-const metasprite_t * const EMOTE_METASPRITE_MIRRORED2[] = {
-    EMOTE_FRAME_MIRRORED2,
+const metasprite_t * const EMOTE_8x16_MIRRORED_METASPRITE2[] = {
+    EMOTE_8x16_MIRRORED_FRAME2,
     NULL
 };
 
-const metasprite_t EMOTE_FRAME3[] = {
+const metasprite_t EMOTE_8x8_FRAME2[] = {
+    METASPR_ITEM(0, 0, 0, 0x02), METASPR_ITEM(0, 8, 2, 0x02),
+    METASPR_ITEM(8, -8, 1, 0x02), METASPR_ITEM(0, 8, 3, 0x02),
+    METASPR_TERM
+};
+const metasprite_t * const EMOTE_8x8_METASPRITE2[] = {
+    EMOTE_8x8_FRAME2,
+    NULL
+};
+const metasprite_t EMOTE_8x8_MIRRORED_FRAME2[] = {
+    METASPR_ITEM(0, 0, 0, 0x02), METASPR_ITEM(0, 8, 0, 0x22),
+    METASPR_ITEM(8, -8, 1, 0x02), METASPR_ITEM(0, 8, 1, 0x22),
+    METASPR_TERM
+};
+const metasprite_t * const EMOTE_8x8_MIRRORED_METASPRITE2[] = {
+    EMOTE_8x8_MIRRORED_FRAME2,
+    NULL
+};
+
+// Frames for palette 3.
+const metasprite_t EMOTE_8x16_FRAME3[] = {
     METASPR_ITEM(0, 0, 0, 0x03), METASPR_ITEM(0, 8, 2, 0x03),
     METASPR_TERM
 };
-const metasprite_t EMOTE_FRAME_MIRRORED3[] = {
+const metasprite_t * const EMOTE_8x16_METASPRITE3[] = {
+    EMOTE_8x16_FRAME3,
+    NULL
+};
+const metasprite_t EMOTE_8x16_MIRRORED_FRAME3[] = {
     METASPR_ITEM(0, 0, 0, 0x03), METASPR_ITEM(0, 8, 0, 0x23),
     METASPR_TERM
 };
-const metasprite_t * const EMOTE_METASPRITE3[] = {
-    EMOTE_FRAME3,
-    NULL
-};
-const metasprite_t * const EMOTE_METASPRITE_MIRRORED3[] = {
-    EMOTE_FRAME_MIRRORED3,
+const metasprite_t * const EMOTE_8x16_MIRRORED_METASPRITE3[] = {
+    EMOTE_8x16_MIRRORED_FRAME3,
     NULL
 };
 
-const metasprite_t EMOTE_FRAME4[] = {
+const metasprite_t EMOTE_8x8_FRAME3[] = {
+    METASPR_ITEM(0, 0, 0, 0x03), METASPR_ITEM(0, 8, 2, 0x03),
+    METASPR_ITEM(8, -8, 1, 0x03), METASPR_ITEM(0, 8, 3, 0x03),
+    METASPR_TERM
+};
+const metasprite_t * const EMOTE_8x8_METASPRITE3[] = {
+    EMOTE_8x8_FRAME3,
+    NULL
+};
+const metasprite_t EMOTE_8x8_MIRRORED_FRAME3[] = {
+    METASPR_ITEM(0, 0, 0, 0x03), METASPR_ITEM(0, 8, 0, 0x23),
+    METASPR_ITEM(8, -8, 1, 0x03), METASPR_ITEM(0, 8, 1, 0x23),
+    METASPR_TERM
+};
+const metasprite_t * const EMOTE_8x8_MIRRORED_METASPRITE3[] = {
+    EMOTE_8x8_MIRRORED_FRAME3,
+    NULL
+};
+
+// Frames for palette 4.
+const metasprite_t EMOTE_8x16_FRAME4[] = {
     METASPR_ITEM(0, 0, 0, 0x04), METASPR_ITEM(0, 8, 2, 0x04),
     METASPR_TERM
 };
-const metasprite_t EMOTE_FRAME_MIRRORED4[] = {
+const metasprite_t * const EMOTE_8x16_METASPRITE4[] = {
+    EMOTE_8x16_FRAME4,
+    NULL
+};
+const metasprite_t EMOTE_8x16_MIRRORED_FRAME4[] = {
     METASPR_ITEM(0, 0, 0, 0x04), METASPR_ITEM(0, 8, 0, 0x24),
     METASPR_TERM
 };
-const metasprite_t * const EMOTE_METASPRITE4[] = {
-    EMOTE_FRAME4,
-    NULL
-};
-const metasprite_t * const EMOTE_METASPRITE_MIRRORED4[] = {
-    EMOTE_FRAME_MIRRORED4,
+const metasprite_t * const EMOTE_8x16_MIRRORED_METASPRITE4[] = {
+    EMOTE_8x16_MIRRORED_FRAME4,
     NULL
 };
 
-const metasprite_t EMOTE_FRAME5[] = {
+const metasprite_t EMOTE_8x8_FRAME4[] = {
+    METASPR_ITEM(0, 0, 0, 0x04), METASPR_ITEM(0, 8, 2, 0x04),
+    METASPR_ITEM(8, -8, 1, 0x04), METASPR_ITEM(0, 8, 3, 0x04),
+    METASPR_TERM
+};
+const metasprite_t * const EMOTE_8x8_METASPRITE4[] = {
+    EMOTE_8x8_FRAME4,
+    NULL
+};
+const metasprite_t EMOTE_8x8_MIRRORED_FRAME4[] = {
+    METASPR_ITEM(0, 0, 0, 0x04), METASPR_ITEM(0, 8, 0, 0x24),
+    METASPR_ITEM(8, -8, 1, 0x04), METASPR_ITEM(0, 8, 1, 0x24),
+    METASPR_TERM
+};
+const metasprite_t * const EMOTE_8x8_MIRRORED_METASPRITE4[] = {
+    EMOTE_8x8_MIRRORED_FRAME4,
+    NULL
+};
+
+// Frames for palette 5.
+const metasprite_t EMOTE_8x16_FRAME5[] = {
     METASPR_ITEM(0, 0, 0, 0x05), METASPR_ITEM(0, 8, 2, 0x05),
     METASPR_TERM
 };
-const metasprite_t EMOTE_FRAME_MIRRORED5[] = {
+const metasprite_t * const EMOTE_8x16_METASPRITE5[] = {
+    EMOTE_8x16_FRAME5,
+    NULL
+};
+const metasprite_t EMOTE_8x16_MIRRORED_FRAME5[] = {
     METASPR_ITEM(0, 0, 0, 0x05), METASPR_ITEM(0, 8, 0, 0x25),
     METASPR_TERM
 };
-const metasprite_t * const EMOTE_METASPRITE5[] = {
-    EMOTE_FRAME5,
-    NULL
-};
-const metasprite_t * const EMOTE_METASPRITE_MIRRORED5[] = {
-    EMOTE_FRAME_MIRRORED5,
+const metasprite_t * const EMOTE_8x16_MIRRORED_METASPRITE5[] = {
+    EMOTE_8x16_MIRRORED_FRAME5,
     NULL
 };
 
-const metasprite_t EMOTE_FRAME6[] = {
+const metasprite_t EMOTE_8x8_FRAME5[] = {
+    METASPR_ITEM(0, 0, 0, 0x05), METASPR_ITEM(0, 8, 2, 0x05),
+    METASPR_ITEM(8, -8, 1, 0x05), METASPR_ITEM(0, 8, 3, 0x05),
+    METASPR_TERM
+};
+const metasprite_t * const EMOTE_8x8_METASPRITE5[] = {
+    EMOTE_8x8_FRAME5,
+    NULL
+};
+const metasprite_t EMOTE_8x8_MIRRORED_FRAME5[] = {
+    METASPR_ITEM(0, 0, 0, 0x05), METASPR_ITEM(0, 8, 0, 0x25),
+    METASPR_ITEM(8, -8, 1, 0x05), METASPR_ITEM(0, 8, 1, 0x25),
+    METASPR_TERM
+};
+const metasprite_t * const EMOTE_8x8_MIRRORED_METASPRITE5[] = {
+    EMOTE_8x8_MIRRORED_FRAME5,
+    NULL
+};
+
+// Frames for palette 6.
+const metasprite_t EMOTE_8x16_FRAME6[] = {
     METASPR_ITEM(0, 0, 0, 0x06), METASPR_ITEM(0, 8, 2, 0x06),
     METASPR_TERM
 };
-const metasprite_t EMOTE_FRAME_MIRRORED6[] = {
+const metasprite_t * const EMOTE_8x16_METASPRITE6[] = {
+    EMOTE_8x16_FRAME6,
+    NULL
+};
+const metasprite_t EMOTE_8x16_MIRRORED_FRAME6[] = {
     METASPR_ITEM(0, 0, 0, 0x06), METASPR_ITEM(0, 8, 0, 0x26),
     METASPR_TERM
 };
-const metasprite_t * const EMOTE_METASPRITE6[] = {
-    EMOTE_FRAME6,
-    NULL
-};
-const metasprite_t * const EMOTE_METASPRITE_MIRRORED6[] = {
-    EMOTE_FRAME_MIRRORED6,
+const metasprite_t * const EMOTE_8x16_MIRRORED_METASPRITE6[] = {
+    EMOTE_8x16_MIRRORED_FRAME6,
     NULL
 };
 
-const metasprite_t EMOTE_FRAME7[] = {
+const metasprite_t EMOTE_8x8_FRAME6[] = {
+    METASPR_ITEM(0, 0, 0, 0x06), METASPR_ITEM(0, 8, 2, 0x06),
+    METASPR_ITEM(8, -8, 1, 0x06), METASPR_ITEM(0, 8, 3, 0x06),
+    METASPR_TERM
+};
+const metasprite_t * const EMOTE_8x8_METASPRITE6[] = {
+    EMOTE_8x8_FRAME6,
+    NULL
+};
+const metasprite_t EMOTE_8x8_MIRRORED_FRAME6[] = {
+    METASPR_ITEM(0, 0, 0, 0x06), METASPR_ITEM(0, 8, 0, 0x26),
+    METASPR_ITEM(8, -8, 1, 0x06), METASPR_ITEM(0, 8, 1, 0x26),
+    METASPR_TERM
+};
+const metasprite_t * const EMOTE_8x8_MIRRORED_METASPRITE6[] = {
+    EMOTE_8x8_MIRRORED_FRAME6,
+    NULL
+};
+
+// Frames for palette 7.
+const metasprite_t EMOTE_8x16_FRAME7[] = {
     METASPR_ITEM(0, 0, 0, 0x07), METASPR_ITEM(0, 8, 2, 0x07),
     METASPR_TERM
 };
-const metasprite_t EMOTE_FRAME_MIRRORED7[] = {
+const metasprite_t * const EMOTE_8x16_METASPRITE7[] = {
+    EMOTE_8x16_FRAME7,
+    NULL
+};
+const metasprite_t EMOTE_8x16_MIRRORED_FRAME7[] = {
     METASPR_ITEM(0, 0, 0, 0x07), METASPR_ITEM(0, 8, 0, 0x27),
     METASPR_TERM
 };
-const metasprite_t * const EMOTE_METASPRITE7[] = {
-    EMOTE_FRAME7,
-    NULL
-};
-const metasprite_t * const EMOTE_METASPRITE_MIRRORED7[] = {
-    EMOTE_FRAME_MIRRORED7,
+const metasprite_t * const EMOTE_8x16_MIRRORED_METASPRITE7[] = {
+    EMOTE_8x16_MIRRORED_FRAME7,
     NULL
 };
 
-const metasprite_t ** const EMOTE_METASPRITES[] = {
-    EMOTE_METASPRITE0,
-    EMOTE_METASPRITE1,
-    EMOTE_METASPRITE2,
-    EMOTE_METASPRITE3,
-    EMOTE_METASPRITE4,
-    EMOTE_METASPRITE5,
-    EMOTE_METASPRITE6,
-    EMOTE_METASPRITE7
+const metasprite_t EMOTE_8x8_FRAME7[] = {
+    METASPR_ITEM(0, 0, 0, 0x07), METASPR_ITEM(0, 8, 2, 0x07),
+    METASPR_ITEM(8, -8, 1, 0x07), METASPR_ITEM(0, 8, 3, 0x07),
+    METASPR_TERM
 };
-const metasprite_t ** const EMOTE_METASPRITES_MIRRORED[] = {
-    EMOTE_METASPRITE_MIRRORED0,
-    EMOTE_METASPRITE_MIRRORED1,
-    EMOTE_METASPRITE_MIRRORED2,
-    EMOTE_METASPRITE_MIRRORED3,
-    EMOTE_METASPRITE_MIRRORED4,
-    EMOTE_METASPRITE_MIRRORED5,
-    EMOTE_METASPRITE_MIRRORED6,
-    EMOTE_METASPRITE_MIRRORED7
+const metasprite_t * const EMOTE_8x8_METASPRITE7[] = {
+    EMOTE_8x8_FRAME7,
+    NULL
+};
+const metasprite_t EMOTE_8x8_MIRRORED_FRAME7[] = {
+    METASPR_ITEM(0, 0, 0, 0x07), METASPR_ITEM(0, 8, 0, 0x27),
+    METASPR_ITEM(8, -8, 1, 0x07), METASPR_ITEM(0, 8, 1, 0x27),
+    METASPR_TERM
+};
+const metasprite_t * const EMOTE_8x8_MIRRORED_METASPRITE7[] = {
+    EMOTE_8x8_MIRRORED_FRAME7,
+    NULL
 };
 
+// Metasprites for all palettes.
+const metasprite_t ** const EMOTE_8x16_METASPRITES[] = {
+    EMOTE_8x16_METASPRITE0,
+    EMOTE_8x16_METASPRITE1,
+    EMOTE_8x16_METASPRITE2,
+    EMOTE_8x16_METASPRITE3,
+    EMOTE_8x16_METASPRITE4,
+    EMOTE_8x16_METASPRITE5,
+    EMOTE_8x16_METASPRITE6,
+    EMOTE_8x16_METASPRITE7
+};
+const metasprite_t ** const EMOTE_8x16_MIRRORED_METASPRITES[] = {
+    EMOTE_8x16_MIRRORED_METASPRITE0,
+    EMOTE_8x16_MIRRORED_METASPRITE1,
+    EMOTE_8x16_MIRRORED_METASPRITE2,
+    EMOTE_8x16_MIRRORED_METASPRITE3,
+    EMOTE_8x16_MIRRORED_METASPRITE4,
+    EMOTE_8x16_MIRRORED_METASPRITE5,
+    EMOTE_8x16_MIRRORED_METASPRITE6,
+    EMOTE_8x16_MIRRORED_METASPRITE7
+};
+
+const metasprite_t ** const EMOTE_8x8_METASPRITES[] = {
+    EMOTE_8x8_METASPRITE0,
+    EMOTE_8x8_METASPRITE1,
+    EMOTE_8x8_METASPRITE2,
+    EMOTE_8x8_METASPRITE3,
+    EMOTE_8x8_METASPRITE4,
+    EMOTE_8x8_METASPRITE5,
+    EMOTE_8x8_METASPRITE6,
+    EMOTE_8x8_METASPRITE7
+};
+const metasprite_t ** const EMOTE_8x8_MIRRORED_METASPRITES[] = {
+    EMOTE_8x8_MIRRORED_METASPRITE0,
+    EMOTE_8x8_MIRRORED_METASPRITE1,
+    EMOTE_8x8_MIRRORED_METASPRITE2,
+    EMOTE_8x8_MIRRORED_METASPRITE3,
+    EMOTE_8x8_MIRRORED_METASPRITE4,
+    EMOTE_8x8_MIRRORED_METASPRITE5,
+    EMOTE_8x8_MIRRORED_METASPRITE6,
+    EMOTE_8x8_MIRRORED_METASPRITE7
+};
+
+// The emote actor and timer.
 actor_t * emote_actor;
 UINT8 emote_timer;
 
@@ -205,9 +389,15 @@ STATIC UINT8 emote_def(UINT16 x, UINT16 y, UINT8 base_tile, BOOLEAN mirrored, UI
     emote_actor->absolute_movement.y = y; // same for `y`, these fields are constant during this emote's raising.
     emote_actor->base_tile           = base_tile;
     emote_actor->sprite_bank         = BANK(VM_EMOTE);
-    emote_actor->sprite_frames       = mirrored ?
-        (metasprite_t **)EMOTE_METASPRITES_MIRRORED[pal] :
-        (metasprite_t **)EMOTE_METASPRITES[pal];
+    if (LCDC_REG & LCDCF_OBJ16) {
+        emote_actor->sprite_frames   = mirrored ?
+            (metasprite_t **)EMOTE_8x16_MIRRORED_METASPRITES[pal] :
+            (metasprite_t **)EMOTE_8x16_METASPRITES[pal];
+    } else {
+        emote_actor->sprite_frames   = mirrored ?
+            (metasprite_t **)EMOTE_8x8_MIRRORED_METASPRITES[pal] :
+            (metasprite_t **)EMOTE_8x8_METASPRITES[pal];
+    }
     emote_actor->animation_interval  = ACTOR_ANIMATION_PAUSED;
 
     return MUL16(EMOTE_SPRITE_TILE_COUNT);
