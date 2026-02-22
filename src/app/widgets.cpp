836b42fadbd88e3cd335b78de9a1365f0670490f
Tony Wang <hellotony@vista.aero>
2026-02-22 20:21:22 +0800

    *Updated window invalidation for dark mode.

--- src/app/widgets.cpp
+++ src/app/widgets.cpp
@@ -27,7 +27,7 @@
 */
 
 #ifndef WIDGETS_ABOUT_REVISION
-#	define WIDGETS_ABOUT_REVISION "r36"
+#	define WIDGETS_ABOUT_REVISION "r37"
 #endif /* WIDGETS_ABOUT_REVISION */
 
 /* ===========================================================================} */
