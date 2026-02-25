512285c63144f9a2633be97450ed8ccd9c891b69
Tony Wang <hellotony@vista.aero>
2026-02-25 19:29:57 +0800

    *Fixed a window refreshing issue under dark mode.

--- src/app/widgets.cpp
+++ src/app/widgets.cpp
@@ -27,7 +27,7 @@
 */
 
 #ifndef WIDGETS_ABOUT_REVISION
-#	define WIDGETS_ABOUT_REVISION "r35"
+#	define WIDGETS_ABOUT_REVISION "r36"
 #endif /* WIDGETS_ABOUT_REVISION */
 
 /* ===========================================================================} */
