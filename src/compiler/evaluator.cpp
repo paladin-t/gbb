613df14dc9a3ad76c03da6ce9724b77c2dfeb850
Tony Wang <hellotony@vista.aero>
2026-04-12 21:00:47 +0800

    +Added file stubs for evaluator.

---
+++ src/compiler/evaluator.cpp
@@ -0,0 +1,20 @@
+/*
+** GB BASIC
+**
+** Copyright (C) 2023-2026 Tony Wang, all rights reserved
+**
+** For the latest info, see https://paladin-t.github.io/kits/gbb/
+*/
+
+#include "evaluator.h"
+
+/*
+** {===========================================================================
+** Evaluator
+*/
+
+namespace GBBASIC {
+
+}
+
+/* ===========================================================================} */
---
+++ src/compiler/evaluator.h
@@ -0,0 +1,29 @@
+/*
+** GB BASIC
+**
+** Copyright (C) 2023-2026 Tony Wang, all rights reserved
+**
+** For the latest info, see https://paladin-t.github.io/kits/gbb/
+*/
+
+#ifndef __EVALUATOR_H__
+#define __EVALUATOR_H__
+
+#include "../gbbasic.h"
+
+/*
+** {===========================================================================
+** Evaluator
+*/
+
+namespace GBBASIC {
+
+class Evaluator {
+public:
+};
+
+}
+
+/* ===========================================================================} */
+
+#endif /* __EVALUATOR_H__ */
