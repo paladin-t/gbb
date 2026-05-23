faa4b718a157f88858bc9f1b6608208869959080
Tony Wang <hellotony@vista.aero>
2026-05-23 18:35:58 +0800

    *Versioning v1.11.0.

--- src/vm/data/builtin.c
+++ src/vm/data/builtin.c
@@ -19,7 +19,7 @@ BANKREF(BUILTIN)
 
 #ifndef GBBVM_VERSION
 #   define GBBVM_VER_MAJOR 1
-#   define GBBVM_VER_MINOR 10
+#   define GBBVM_VER_MINOR 11
 #   define GBBVM_VER_REVISION 0
 #endif /* GBBVM_VERSION */
 
