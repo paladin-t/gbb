fe4674a881030f52afdba48482494eec5c1b3bb0
Tony Wang <hellotony@vista.aero>
2026-06-23 13:58:42 +0800

    +Added a `blit_scene` native function.

--- src/vm/vm_scene.c
+++ src/vm/vm_scene.c
@@ -249,8 +249,33 @@ void scene_get_trigger(
     *cb_address  = get_ptr  (scene.trigger_bank, data  ); data += 2;
 }
 
-// Shake camera.
-UINT8 camera_shake(POINTER THIS, UINT8 start, UINT16 * stack_frame) OLDCALL BANKED { // INVOKABLE.
+// Transfers a region of scene data to the current scene.
+BOOLEAN scene_blit(POINTER THIS, UINT8 start, UINT16 * stack_frame) OLDCALL BANKED { // INVOKABLE.
+    (void)THIS; (void)start;
+
+    const UINT8 x = (UINT8)stack_frame[3];
+    const UINT8 y = (UINT8)stack_frame[2];
+    const UINT8 w = (UINT8)stack_frame[1];
+    const UINT8 h = (UINT8)stack_frame[0];
+
+    if (scene.map_bank == 0)
+        return TRUE;
+
+    SCENE_LOAD(
+        scene.map_bank, scene.map_address,
+        scene.attr_bank, scene.attr_address,
+        scene_map_x + x, scene_map_y + y,
+        w, h,
+        scene.width,
+        scene.base_tile,
+        set_bkg_submap
+    );
+
+    return TRUE;
+}
+
+// Shakes camera.
+BOOLEAN camera_shake(POINTER THIS, UINT8 start, UINT16 * stack_frame) OLDCALL BANKED { // INVOKABLE.
     if (start) *((SCRIPT_CTX *)THIS)->stack_ptr = sys_time;
     if (((UINT16)sys_time - *((SCRIPT_CTX *)THIS)->stack_ptr) < stack_frame[1]) {
         if (stack_frame[0] & SCENE_CAMERA_SHAKE_X) {
