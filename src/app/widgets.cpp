cdd618345f885494392b4764f77b8f4ca2a1b71e
Tony Wang <hellotony@vista.aero>
2026-07-09 18:05:56 +0800

    *Revisioning r50.

--- src/app/widgets.cpp
+++ src/app/widgets.cpp
@@ -27,7 +27,7 @@
 */
 
 #ifndef WIDGETS_ABOUT_REVISION
-#	define WIDGETS_ABOUT_REVISION "r49"
+#	define WIDGETS_ABOUT_REVISION "r50"
 #endif /* WIDGETS_ABOUT_REVISION */
 
 /* ===========================================================================} */
