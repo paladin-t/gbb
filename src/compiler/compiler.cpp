98b35a07c474c8db582078a6bf1524ab742d66e2
Tony Wang <hellotony@vista.aero>
2026-07-19 21:59:28 +0800

    *Emit `data arb(...)` as 16-bit integer.

--- src/compiler/compiler.cpp
+++ src/compiler/compiler.cpp
@@ -34607,70 +34607,75 @@ private:
 					}
 					q1.tokens.push_back(tk);
 				} else if (ignore(Token::Types::KEYWORD, "arb")(q1)) {
-					Token::Ptr tk = nullptr;
 					int arb = -1;
 					Token::Ptr id0 = nullptr;
 					Token::Ptr id1 = nullptr;
 					if (!ignore(Token::Types::OPERATOR, "(")(q1))
 						return false;
-					if (!forward(Token::Types::OPERATOR, ")")(q1.index)) {
-						if (forwardN(2, Token::Types::OPERATOR, ",")(q1.index)) {
-							// `=ARB(#pg|"{name}", ch)`.
-							Variant arg0 = nullptr;
-							Variant arg1 = nullptr;
-							if ((id0 = ignore(Token::Types::STRING)(q1))) {
-								arg0 = id0->data();
-							} else if ((id0 = ignore(Token::Types::PAGE)(q1))) {
-								std::string pg = (std::string)id0->text();
-								if (pg.empty() || pg.front() != '#') {
-									GBBASIC_ASSERT(false && "Impossible.");
+					if (forward(Token::Types::OPERATOR, ")")(q1.index)) return false;
+					if (forwardN(2, Token::Types::OPERATOR, ",")(q1.index)) {
+						// `=ARB(#pg|"{name}", ch)`.
+						Variant arg0 = nullptr;
+						Variant arg1 = nullptr;
+						if ((id0 = ignore(Token::Types::STRING)(q1))) {
+							arg0 = id0->data();
+						} else if ((id0 = ignore(Token::Types::PAGE)(q1))) {
+							std::string pg = (std::string)id0->text();
+							if (pg.empty() || pg.front() != '#') {
+								GBBASIC_ASSERT(false && "Impossible.");
 
-									return false;
-								}
-								pg.erase(pg.begin());
-								int pgn = -1;
-								if (!Text::fromString(pg, pgn))
-									return false;
+								return false;
+							}
+							pg.erase(pg.begin());
+							int pgn = -1;
+							if (!Text::fromString(pg, pgn))
+								return false;
 
-								arg0 = Variant(pgn);
-							}
-							if (!ignore(Token::Types::OPERATOR, ",")(q1)) return false;
-							if ((id1 = ignore(Token::Types::STRING)(q1))) {
-								arg1 = id1->data();
-							} else if ((id1 = ignore(Token::Types::INTEGER)(q1))) {
-								arg1 = id1->data();
-							}
+							arg0 = Variant(pgn);
+						}
+						if (!ignore(Token::Types::OPERATOR, ",")(q1)) return false;
+						if ((id1 = ignore(Token::Types::STRING)(q1))) {
+							arg1 = id1->data();
+						} else if ((id1 = ignore(Token::Types::INTEGER)(q1))) {
+							arg1 = id1->data();
+						}
+						if (!arbLookup(arb, arg0, arg1, id0 ? id0->begin() : TextLocation::INVALID(), id1 ? id1->begin() : TextLocation::INVALID())) {
+							arg1 = Variant(std::string("?"));
 							if (!arbLookup(arb, arg0, arg1, id0 ? id0->begin() : TextLocation::INVALID(), id1 ? id1->begin() : TextLocation::INVALID())) {
-								arg1 = Variant(std::string("?"));
-								if (!arbLookup(arb, arg0, arg1, id0 ? id0->begin() : TextLocation::INVALID(), id1 ? id1->begin() : TextLocation::INVALID())) {
+								arb = -1;
+							}
+						}
+					} else {
+						// `=ARB(ch)`.
+						if ((id0 = ignore(Token::Types::STRING)(q1))) {
+							Variant arg0 = id0->data();
+							if (!arbLookup(arb, nullptr, arg0, id0 ? id0->begin() : TextLocation::INVALID(), id0 ? id0->begin() : TextLocation::INVALID())) {
+								arg0 = Variant(std::string("?"));
+								if (!arbLookup(arb, nullptr, arg0, id0 ? id0->begin() : TextLocation::INVALID(), id0 ? id0->begin() : TextLocation::INVALID())) {
 									arb = -1;
 								}
 							}
 						} else {
-							// `=ARB(ch)`.
-							if ((id0 = ignore(Token::Types::STRING)(q1))) {
-								Variant arg0 = id0->data();
-								if (!arbLookup(arb, nullptr, arg0, id0 ? id0->begin() : TextLocation::INVALID(), id0 ? id0->begin() : TextLocation::INVALID())) {
-									arg0 = Variant(std::string("?"));
-									if (!arbLookup(arb, nullptr, arg0, id0 ? id0->begin() : TextLocation::INVALID(), id0 ? id0->begin() : TextLocation::INVALID())) {
-										arb = -1;
-									}
-								}
-							} else {
-								return false;
-							}
+							return false;
 						}
 					}
+					if (!ignore(Token::Types::OPERATOR, ")")(q1)) return false;
 
-					tk = Token::Ptr(new Token());
+					Token::Ptr tky(new Token());
+					tky
+						->type(Token::Types::KEYWORD)
+						->data("int")
+						->text("int")
+						->begin(id0 ? id0->begin() : TextLocation::INVALID())
+						->end(id0 ? id0->end() : TextLocation::INVALID());
+					Token::Ptr tk(new Token());
 					tk
 						->type(Token::Types::INTEGER)
 						->data(arb)
 						->text(Text::toString(arb))
 						->begin(id0 ? id0->begin() : TextLocation::INVALID())
 						->end(id0 ? id0->end() : TextLocation::INVALID());
-
-					if (!ignore(Token::Types::OPERATOR, ")")(q1)) return false;
+					q1.tokens.push_back(tky);
 					q1.tokens.push_back(tk);
 				} else {
 					break;
