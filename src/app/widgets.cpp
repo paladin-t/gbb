99f81e6ca2c2b6b06c52ffd48e8f4ec8ee62e5e9
Tony Wang <hellotony@vista.aero>
2026-08-01 18:13:59 +0800

    *Revisioning r53.

--- src/app/widgets.cpp
+++ src/app/widgets.cpp
@@ -27,7 +27,7 @@
 */
 
 #ifndef WIDGETS_ABOUT_REVISION
-#	define WIDGETS_ABOUT_REVISION "r52"
+#	define WIDGETS_ABOUT_REVISION "r53"
 #endif /* WIDGETS_ABOUT_REVISION */
 
 /* ===========================================================================} */
